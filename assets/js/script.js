25360d96c7b0e147f92570c0516ffc95a879bf09
Sean L <leonardmsean@gmail.com>
2023-03-06 12:25:13 -0500

    added a lot of code over several days.  I should have commited A LOT more.  learn from this.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,43 @@
 
-var landingPage = document.getElementsByClassName("landing-page"); 
-var quizPage = document.getElementsByClassName("quiz-page");
-var endPage = document.getElementsByClassName("final-page");
-var startButton = document.getElementsById("start-button");
-var submitName = document.getElementsById("submit-name");
-var userScore = document.getElementsById("score");
-var userNameInput = document.getElementById("name-input");
+var landingPage = document.querySelector(".landing-page"); 
+var quizPage = document.querySelector(".quiz-page");
+var endPage = document.querySelector(".final-page");
+var startButton = document.querySelector(".start-button");
+var submitName = document.querySelector(".submit-name");
+var userScore = document.querySelector(".score");
+var userNameInput = document.querySelector(".name-input");
+var highScores = document.querySelector(".high-scores");
 
 
 
 
+function initQuiz() {
+  landingPage.style.display = "flex";
+  quizPage.style.display = "none";
+  endPage.style.display = "none";
+  highScores.style.display = "none";
+}
 
 
+window.onload = initQuiz;
 
+var timerElement = document.getElementById('timer');
+let timeLeft = 250;
+let timerInterval;
+
+startButton.addEventListener("click", function () {
+  landingPage.style.display = "none";
+  quizPage.style.display = "block";
+  showQuestion();
+  timerInterval = setInterval(function() {
+    timeLeft--;
+    timerElement.textContent = "Time left: " + timeLeft + " seconds";
+    if (timeLeft <= 0) {
+      clearInterval(timerInterval);
+      endQuiz();
+    }
+  }, 1000);
+});
 
 var quizQuestions = [
     {
@@ -130,3 +155,63 @@ var quizQuestions = [
         correctOptionIndex: 2
       },
      ];
+
+     var currentQuestion = 0;
+
+
+
+     function showQuestion() {
+        var questions = document.querySelector("h2");
+        var answers = document.querySelector("#answer-options");
+        var current = quizQuestions[currentQuestion];
+      questions.textContent = current.question;   
+      answers.innerHTML = "";
+
+    for (var i = 0; i < current.options.length; i++) {
+      var option = current.options[i];
+      var radio = document.createElement("input");
+          radio.type = "radio";
+          radio.name = "answer";
+          radio.value = option;
+          
+          var label = document.createElement("label");
+          label.textContent = option;
+          
+          var li = document.createElement("li");
+          li.appendChild(radio);
+          li.appendChild(label);
+
+          answers.appendChild(li);
+
+          li.addEventListener("click", function() {
+            radio.click();
+          });
+
+          radio.addEventListener("click", function() {
+            if (this.value === current.options[current.correctOptionIndex]) {
+              li.classList.add("correct");
+            } else {
+              li.classList.add("incorrect");
+            }       
+        
+        for (var j = 0; j < answers.children.length; j++) {
+          answers.children[j].removeEventListener("click", arguments.callee);
+        }
+
+        currentQuestion++;
+        if (currentQuestion < quizQuestions.length) {
+          showQuestion();
+        } else {
+          showendPage();
+        }
+        });
+        
+      }
+      var totalQuestions = 25;
+      var questonCounter = document.getElementById("question-counter");
+      var curQues = currentQuestion + 1;
+      questonCounter.textContent = "Question " + curQues + " of " + totalQuestions;
+    }
+     
+    
+  
